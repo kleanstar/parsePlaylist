2e40bc2cdd69d46ab50d8ec352bb19fe90ea2679
Angel Mitchell <angel.mitchell@nbcuni.com>
2022-03-02 00:35:23 -0600

    Initial Files

---
+++ parsePlaylist.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+
+/*
+* read in file 
+* parse media instances
+* sort by attributes
+*/
+class masterPlaylist {
+    // read in file line by line starting with #
+
+
+}
+
+
+/*
+ * instance of each media manifest
+*/
+class Media {
+
+}
+
+
+// main() is where program execution begins.
+int main() {
+   cout << "Hello World"; // prints Hello World
+   return 0;
+}
