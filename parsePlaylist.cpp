52cef62fce99ebf6c69be1b76c16394e6cf50495
Angel Mitchell <angel.mitchell@nbcuni.com>
2022-03-02 12:51:39 -0600

    Master Design Comments

--- parsePlaylist.cpp
+++ parsePlaylist.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
 
+/**
+ * @brief 
+ * EXTM3U
+ * EXT-X-VERSION
+ * EXT-X-SESSION-DATA
+ * EXT-X-SESSION-KEY
+ * EXT-X-INDEPENDENT-SEGMENTS
+ * EXT-X-START
+ * EXT-X-MEDIA
+ * EXT-X-STREAM-INF
+ * EXT-X-I-FRAME-STREAM-INF
+ */
 
 /*
 * read in file 
@@ -9,21 +21,38 @@ using namespace std;
 */
 class masterPlaylist {
     // read in file line by line starting with #
+    // remove blank line & whitspace
+
+    // validate Master plylist rules
+
+    // Global vs Instance Tags ":"
+
+    // pull in each tagged instance & attributes 
+
+    // parse by "," delimiter excluding CODECS ","
+
+    // create key:value pairs for each "=" within
 
 
 }
 
 
 /*
- * instance of each media manifest
+ * instance of each media manifest rendition
 */
-class Media {
+class MediaRendition {
+
+}
+
+/*
+ * instance of each variant stream 
+*/
+class MediaVariant {
 
 }
 
 
 // main() is where program execution begins.
 int main() {
-   cout << "Hello World"; // prints Hello World
    return 0;
 }
